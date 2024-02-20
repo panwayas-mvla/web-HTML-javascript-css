37e9b16cde2afc97353228b16d17c059c844eb44
panwayas-mvla <100028583@mvla.net>
2024-02-19 16:18:29 -0800

    Organized

--- jest-testing-lab/jest-testing/timesTwo.test.js
+++
@@ -1,7 +0,0 @@
-const {default: TestRunner} = require("jest-runner");
-const timesTwo = require('./timesTwo');
-
-// Write the first test
-test('returns the number times 2', () => {
-    expect(timesTwo(10)).toBe(20)
-});
