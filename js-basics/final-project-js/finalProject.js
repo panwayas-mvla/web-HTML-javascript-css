bca8f1d3886008aa7497d3dfc00534f186971feb
panwayas-mvla <100028583@mvla.net>
2024-02-19 16:13:19 -0800

    For all css basic projects from coursera

---
+++ js-basics/final-project-js/finalProject.js
@@ -0,0 +1,77 @@
+// Given variables
+const dishData = [
+    {
+        name: "Italian pasta",
+        price: 9.55
+    },
+    {
+        name: "Rice with veggies",
+        price: 8.65
+    },
+    {
+        name: "Chicken with potatoes",
+        price: 15.55
+    },
+    {
+        name: "Vegetarian Pizza",
+        price: 6.45
+    },
+]
+const tax = 1.20;
+
+// Implement getPrices()
+function getPrices(taxBoolean) {
+    dishData.forEach((data) => {
+        var finalPrice;
+        if(taxBoolean) 
+            finalPrice = data.price * tax;
+        else if(taxBoolean == false)
+            finalPrice = data.price;
+        else {
+            console.log('You need to pass a boolean to the getPrices call!');
+            return;
+        }
+        console.log(`Dish: ${data.name}`);
+        console.log(`Price: $ ${finalPrice}`);
+    });
+    /*
+    for (menu of dishData) {
+      // console.log(menu); this is for testing the code
+      let finalPrice;
+      if (taxBoolean == true) {
+        finalPrice = menu.price * tax;
+      } else if(taxBoolean == false){
+        finalPrice = menu.price;
+      }
+      else {
+        console.log("You need to pass a boolean to the getPrices call!");
+        return;
+      }
+  
+      console.log(`Dish: ${menu.name} Price: $${finalPrice}`);
+      // console.log("Dish: " + menu.name + "Price: $" + finalPrice)
+    }
+    */
+   //alternative for each loop
+}
+
+// Implement getDiscount()
+function getDiscount(taxBoolean, guests) {
+    getPrices(taxBoolean);
+    if((typeof(guests) == "number") && (guests > 0 && guests < 30)) { //number and (0,3)
+        let discount = 0;
+        if(guests < 5) 
+            discount = 5;
+        else if(guests >= 5)
+            discount = 10;
+        console.log('Discount is: $' + discount);
+    } else {
+        console.log('The second argument must be a number between 0 and 30');
+    }
+}
+
+// Call getDiscount()
+getDiscount(true, 2);
+getDiscount(false, 10);
+
+
